17cfb5dbd27148c029e6b96243e12a14beee0a11
Quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2017-10-28 12:26:31 +0700

    initial all files

---
+++ app/src/main/java/vnuk/edu/vn/cse15/vnuk_lms/MainActivity.java
@@ -0,0 +1,13 @@
+package vnuk.edu.vn.cse15.vnuk_lms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
